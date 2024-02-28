31e00b0d74a00ab3f2f79321abba630d7abea5b8
Oscar Guillermo Flores Balladares <130212627+okytek@users.noreply.github.com>
2024-02-27 23:15:17 -0500

    Add files via upload

---
+++ Master02-Autoinstalador_de_Docker_y_Kubernetes_para_CBL-Mariner.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Solicita al usuario que ingrese la dirección IP del servidor
+read -p "Ingrese la dirección IP del servidor (por ejemplo, 192.168.0.101): " direccion_ip
+
+# Inicializa el clúster Kubernetes con la dirección IP proporcionada
+sudo kubeadm init --apiserver-advertise-address=$direccion_ip --ignore-preflight-errors=all
+
+# Configura el directorio .kube para el usuario actual
+mkdir -p $HOME/.kube
+
+# Copia el archivo de configuración del clúster a la ubicación del usuario
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+
+# Asigna los permisos adecuados al archivo de configuración
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
+# Aplica la configuración estándar de la API Gateway
+kubectl apply -f https://github.com/kubernetes-sigs/gateway-api/releases/download/v1.0.0/standard-install.yaml
+
+# Descarga el archivo de configuración de los componentes del servidor de métricas
+wget https://github.com/kubernetes-sigs/metrics-server/releases/latest/download/components.yaml
+
+# Modifica el archivo de configuración para permitir conexiones inseguras al Kubelet
+sed -i 's/        - --metric-resolution=15s/        - --metric-resolution=15s\n        - --kubelet-insecure-tls=true/g' components.yaml
+
+# Aplica la configuración del servidor de métricas
+kubectl apply -f components.yaml
+
+# Elimina el archivo temporal de configuración
+rm -rf components.yaml
+
+# Quita las restricciones de roles en todos los nodos del clúster
+kubectl taint nodes --all node-role.kubernetes.io/control-plane-
+kubectl taint nodes --all node-role.kubernetes.io/master-
---
+++ Worker01-Autoinstalador_de_Docker_y_Kubernetes_para_CBL-Mariner.sh
@@ -0,0 +1,106 @@
+#!/bin/bash
+
+# Solicitar al usuario el nombre del host
+read -p "Ingresa el nombre del host (por ejemplo, vm2.192.168.0.102.nip.io): " nombre_host
+
+# Establecer el nombre del host
+hostnamectl set-hostname "$nombre_host" --static
+
+# Actualizar la configuración en /etc/hosts con el nuevo nombre del host
+sed -i "s/127.0.0.1   $HOSTNAME/127.0.0.1   $nombre_host/g" /etc/hosts
+
+# Solicitar la dirección IP, máscara de red y puerta de enlace al usuario
+read -p "Ingresa la dirección IP y la máscara de red (por ejemplo, 192.168.0.102/24): " direccion_ip_mascara_red
+read -p "Ingresa la puerta de enlace (por ejemplo, 192.168.0.1): " puerta_enlace
+
+# Configurar la interfaz de red estática usando systemd
+cat > /etc/systemd/network/01-static-en.network << EOF
+[Match]
+Name=eth0
+
+[Network]
+Address=$direccion_ip_mascara_red
+Gateway=$puerta_enlace
+EOF
+
+# Establecer permisos para el archivo de configuración
+chmod 644 /etc/systemd/network/01-static-en.network
+
+# Configurar servidores de nombres en /etc/resolv.conf
+servidores_nombres=()
+while true; do
+    read -p "Ingresa un servidor de nombres (por ejemplo, 190.113.220.18 luego 190.113.220.51 y finalmente 190.113.220.54): " servidor_nombres
+    servidores_nombres+=("nameserver $servidor_nombres\n")
+    read -p "¿Deseas ingresar otro servidor de nombres? (s/n): " continuar
+    if [ "$continuar" != "s" ]; then
+        break
+    fi
+done
+cadena_servidores_nombres=$(IFS=''; echo "${servidores_nombres[*]}")
+sed -i "s/nameserver 127.0.0.53/$cadena_servidores_nombres/g" /etc/resolv.conf
+
+# Configurar la cadena de búsqueda en /etc/resolv.conf
+read -p "Ingresa la cadena de búsqueda (por ejemplo, 192.168.0.102.nip.io): " cadena_busqueda
+sed -i "s/search ./search $cadena_busqueda/g" /etc/resolv.conf
+
+# Deshabilitar e detener el servicio iptables
+systemctl disable iptables
+systemctl stop iptables
+
+# Cargar módulos del kernel para overlay y br_netfilter
+modprobe overlay
+modprobe br_netfilter
+
+# Habilitar el reenvío de IP en el kernel
+sed -i 's/net.ipv4.ip_forward = 0/net.ipv4.ip_forward = 1/g' /etc/sysctl.d/50-security-hardening.conf
+
+# Configurar opciones de red para iptables
+cat > /etc/sysctl.d/01-K8s.conf << EOF
+net.bridge.bridge-nf-call-iptables = 1
+EOF
+
+# Aplicar configuraciones del kernel
+sysctl --system
+
+# Instalar paquetes necesarios utilizando tdnf
+tdnf -y install conntrack-tools dnsmasq jq kubernetes-client kubernetes-kubeadm moby-cli moby-engine
+
+# Eliminar un archivo de configuración no necesario
+rm -rf /etc/cni/net.d/99-loopback.conf.sample
+
+# Configurar la red para el contenedor Docker
+cat > /etc/cni/net.d/01-bridge.conf << EOF
+{
+    "cniVersion": "0.4.0",
+    "name": "br",
+    "type": "bridge",
+    "bridge": "cni0",
+    "isGateway": true,
+    "ipMasq": true,
+    "ipam": {
+        "type": "host-local",
+        "subnet": "10.1.0.0/16",
+        "routes": [
+        { "dst": "0.0.0.0/0" }
+        ]
+    }
+}
+EOF
+
+# Configurar la interfaz loopback para el contenedor Docker
+cat > /etc/cni/net.d/99-loopback.conf << EOF
+{
+    "cniVersion": "0.4.0",
+    "name": "lo",
+    "type": "loopback"
+}
+EOF
+
+# Habilitar y arrancar el servicio Docker
+systemctl enable --now docker.service
+
+# Agregar al usuario actual al grupo Docker
+usermod -aG docker "$USER"
+
+# Reiniciar el sistema para aplicar los cambios
+reboot
